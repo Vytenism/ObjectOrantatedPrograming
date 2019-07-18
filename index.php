49f3819d1155f096040ffa8cbdd703a7b1d05c00
vytenism <aparance23@gmail.com>
2019-07-18 13:15:31 +0300

    1oop

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
 class FileDB {
     private $file_name;
-    
-    public function __construct($file_name) {
+    private $data;
+    public function __construct($file_name, $data) {
         $this->file_name = $file_name;
+        $this->data = $data;
+    }
+    public function getData(){
+        $this->data = file_get_contents($file_name);
+    }
+    public function setData(){
+        $this->data = file_put_contents($file_name);
+    }
+    public function load(){
+        $this->data = json_decode($data);
+    }
+    public function save(){
+        $this->data = json_encode($data);
     }
 }
 
