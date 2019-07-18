cad817bd5c5845b8096263d02f12e1e2e1ca0d2a
vytenism <aparance23@gmail.com>
2019-07-18 12:13:50 +0300

    1oop

--- index.php
+++ index.php
@@ -2,17 +2,21 @@
 
 class ThailandSurprise{
     public $clothes;
-    public $qty;
     private $balls;
     public function __construct($clothes){
         $this->clothes = $clothes;
         $this->balls = rand(0, 1);
     }
-//    public function use_item(){
-//        $this->qty--;
-//    }
+    public function attachBalls(){
+        $this->balls= true;
+    }
+    public function detachBalls(){
+        $this->balls= false;
+    }
 }
 $name = new ThailandSurprise('miniskirt');
-print $name->clothes;
+$name->attachBalls();
+
+//print $name->clothes;
 
 var_dump($name);
