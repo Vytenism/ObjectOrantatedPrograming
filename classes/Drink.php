904a0b1d86692f40c33ecbc74f517e65e0c623fe
vytenism <aparance23@gmail.com>
2019-07-23 13:21:01 +0300

    3oop

---
+++ classes/Drink.php
@@ -0,0 +1,56 @@
+<?php 
+
+declare (strict_type = 1);
+
+class Drink{
+    
+    private $data;
+    private $name;
+    private $amount_ml;
+    private $abarot;
+    private $image;
+    
+    public function __construct($data) {
+        $this->data = $data;
+    }
+    
+    public function setName (string $name){
+        $this->data['name'] = $name;
+    }
+    
+    public function getName (){
+        if($this->data['name']){
+            return $this->data['name'];
+        }
+    }
+
+    public function setAmount_ml (int $amount_ml){
+        $this->data['amount_ml'] = $amount_ml;
+    }
+    
+    public function getAmount_ml (){
+        if ($this->data['amount_ml']){
+            return $this->data['amount_ml'];
+        }
+    }
+    
+    public function setAbarot(float $abarot){
+        $this->data['abarot'] = $abarot;
+    }
+    
+    public function getAbarot(){
+        if($this->data['abarot']){
+            return $this->data['abarot'];
+        }
+    }
+    
+    public function setImage(string $image){
+        $this->data['image'] = $image;
+    }
+    
+    public function getImage(){
+        if($this->data['image']){
+            return $this->data['image'];
+        }
+    }
+} 
