dac35ce982e512e122a2d5d436c91203d9259cfe
vytenism <aparance23@gmail.com>
2019-07-24 12:50:43 +0300

    OOP1

--- app/classes/Drinks/Model.php
+++ app/classes/Drinks/Model.php
@@ -4,12 +4,11 @@ namespace App\Drinks;
 
 class Model{
     
-    private $fileDb;
     private $table_name;
     
     public function __construct($table_name) {
-        $object = new \Core\FileDB($table_name);
-        $this->fileDb = $object->load();
+        $object = new \Core\FileDB(DB_FILE);
+        $this->table_name = $object->load();
         $this->table_name = $object->createTable($table_name);
     }
 }
